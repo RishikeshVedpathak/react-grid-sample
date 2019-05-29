5159cbcdb815b6a2e6e342c2db02f5f149da43c5
rishivedpathak <rishikeshvedpathak@gmail.com>
2019-05-29 19:41:08 +0530

    demo ready commit

--- src/containers/Dashboard/Dashboard.js
+++ src/containers/Dashboard/Dashboard.js
@@ -1,10 +1,14 @@
-import React, {Component} from "react";
-import styles from "./Dashboard.css";
+import React, { Component } from "react";
+import styles from "./Dashboard.module.scss";
+
+import DataTable from "../../components/DataTable/DataTable";
 
 class Dashboard extends Component {
-  render(){
-    return(
-      <div className={styles.Warapper}>{JSON.stringify(styles)}Dashboard Container</div>
+  render() {
+    return (
+      <div className={styles.Warapper}>
+        <DataTable />
+      </div>
     )
   }
 };
